6f84cd569e39e7b7c56191f855031574cd3a3f84
ShakilTalukder07 <shakil.talukder07@gmail.com>
2022-11-08 00:37:14 +0600

    banner add

--- src/shared/Header/Header.js
+++ src/shared/Header/Header.js
@@ -1,7 +1,6 @@
 import { Link } from 'react-router-dom';
 
 
-
 const Header = () => {
 
 
@@ -22,7 +21,7 @@ const Header = () => {
 
     return (
         <div>
-            <div className="navbar h-20 mb-12 pt-12 bg-base-100">
+            <div className="navbar h-20 mb-8 pt-8 shadow-sm bg-base-100">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
