9ab00696e5695596c39d610740262f4df2a342d9
ShakilTalukder07 <shakil.talukder07@gmail.com>
2022-11-10 03:04:55 +0600

    about us page add

--- src/Pages/Home/AboutUS/AboutUS.js
+++ src/Pages/Home/AboutUS/AboutUS.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import aboutImg from '../../../assets/images/aboutUsImages/aboutImg.jpg'
 
 const AboutUS = () => {
     return (
         <div className='my-8'>
             <div className="hero min-h-screen bg-base-200">
                 <div className="hero-content flex-col lg:flex-row">
-                    <img src="https://placeimg.com/260/400/arch" alt='/' className="max-w-sm rounded-lg shadow-2xl" />
-                    <div>
+                    <img src={aboutImg} alt='/' className="max-w-sm rounded-lg shadow-2xl" />
+                    <div className='ml-6'>
                         <h1 className="text-5xl font-bold">About US</h1>
-                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <p className="py-6">Paradise is an tourism focused service. <br />
+                            Paradise provide various types of services with minimal cost. <br />
+                            Such as Flight Reservation Booking, Tour Visa, Hotel Booking, <br />
+                            Delivery service, Passport Consultancy and many more. </p>
                     </div>
                 </div>
             </div>
